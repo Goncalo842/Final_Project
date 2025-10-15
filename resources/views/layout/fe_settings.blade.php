063ad3b07b6b68ca01a3196f8667816f1fdddf5b
Goncalo842 <gdinisqueiros@gmail.com>
2025-10-16 00:29:25 +0100

    config

--- resources/views/layout/fe_settings.blade.php
+++ resources/views/layout/fe_settings.blade.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="pt-PT">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ISTP - University</title>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/settings.css') }}">
-    <title>Document</title>
 </head>
-
 <body>
 
     <div class="container bar-container">
@@ -58,4 +56,5 @@
     @yield('content')
 
 </body>
+
 </html>
